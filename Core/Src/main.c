5ace224b2c3f13934e5050f8c3415a91f8087c99
Adham Hegazy <83841230+AdhamHegazy@users.noreply.github.com>
2022-05-06 22:26:39 +0200

    Milestone 2

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -42,6 +42,7 @@
 /* Private variables ---------------------------------------------------------*/
 SPI_HandleTypeDef hspi3;
 
+UART_HandleTypeDef huart1;
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
@@ -53,13 +54,14 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_SPI3_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+char Buffer [1];
 /* USER CODE END 0 */
 
 /**
@@ -89,23 +91,29 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
-  	  MX_GPIO_Init();
-  	  MX_SPI3_Init();
-  	  MX_USART2_UART_Init();
-
+  MX_GPIO_Init();
+  MX_SPI3_Init();
+  MX_USART2_UART_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
-  	  PN532 pn532;
-  	  PN532_SPI_Init(&pn532);
 
-	  while(PN532_CallFunction(&pn532, PN532_COMMAND_GETFIRMWAREVERSION, NULL, 4, NULL, 0, 500) != PN532_STATUS_ERROR)
-	  {
-		HAL_UART_Transmit(&huart2,"\rCould not connect to the RFID sensor\r\n",sizeof("\rCould not connect to the RFID sensor\r\n"), 10000);
-		HAL_Delay(1000);
-	  }
+  //NFC config
+  PN532 pn532;
+  PN532_SPI_Init(&pn532);
 
-	  HAL_UART_Transmit(&huart2,"\rConnected to sensor successfully\n\r",sizeof("\rConnected to sensor successfully\n\r"), 10000);
+  while(PN532_CallFunction(&pn532, PN532_COMMAND_GETFIRMWAREVERSION, NULL, 4, NULL, 0, 500) != PN532_STATUS_ERROR)
+  {
+	HAL_UART_Transmit(&huart2,"\rCould not connect to the RFID sensor\r\n",sizeof("\rCould not connect to the RFID sensor\r\n"), 10000);
+	HAL_Delay(1000);
+  }
 
-	  PN532_SamConfiguration(&pn532);
+  HAL_UART_Transmit(&huart2,"\rConnected to sensor successfully\n\r",sizeof("\rConnected to sensor successfully\n\r"), 10000);
+
+  PN532_SamConfiguration(&pn532);
+
+  //Enable IT receive
+  HAL_UART_Receive_IT (&huart1, Buffer, 1);
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -119,18 +127,22 @@ int main(void)
 
 	    if (Buffer_Size != -1)
 	    {
-			HAL_UART_Transmit(&huart2, "ID Detected: ",sizeof("ID Detected: "), 10000);
+			unsigned char ID[8];
 
 			for (int i = 0; i<Buffer_Size; i++)
-			{
-				unsigned char hex[2];
-				sprintf(hex, "%02X ", (unsigned char)Buffer[i]);
-				HAL_UART_Transmit(&huart2,hex,sizeof(hex), 10000);
-			}
+				sprintf(ID + 2*i, "%02X ", (unsigned char)Buffer[i]);
 
-		HAL_UART_Transmit(&huart2, "\r\n",sizeof("\r\n"), 10000);
+
+
+			HAL_UART_Transmit(&huart2, "ID Detected: ",sizeof("ID Detected: "), 10000);
+			HAL_UART_Transmit(&huart2,ID,sizeof(ID), 10000);
+			HAL_UART_Transmit(&huart2, "\r\n",sizeof("\r\n"), 10000);
+
+			HAL_UART_Transmit(&huart1,ID,sizeof(ID), 10000);
 
 	    }
+
+	    HAL_Delay(100);
 
 
     /* USER CODE END WHILE */
@@ -229,6 +241,41 @@ static void MX_SPI3_Init(void)
 }
 
 /**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
   * @brief USART2 Initialization Function
   * @param None
   * @retval None
@@ -289,7 +336,15 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+	if (Buffer [0] == '1')
+		HAL_UART_Transmit(&huart2,"User Found\r\n",sizeof("User Found\r\n"), 10000);
+	else if (Buffer[0] == '0')
+		HAL_UART_Transmit(&huart2,"User Not Found\r\n",sizeof("User Not Found\r\n"), 10000);
 
+	HAL_UART_Receive_IT (&huart1, Buffer, 1);
+}
 /* USER CODE END 4 */
 
 /**
